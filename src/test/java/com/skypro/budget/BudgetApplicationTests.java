1b7a6a2753336a28b38288e8dd99c392db6e6746
JohanNayvert <presnetsov.evgeniy@gmail.com>
2023-01-08 11:29:49 +0500

    First commit

---
+++ src/test/java/com/skypro/budget/BudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
